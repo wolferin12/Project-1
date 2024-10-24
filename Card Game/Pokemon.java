9d9b42fca9edce910e9ea030c2205ef271d2e4a4
wolferin12 <95312861+wolferin12@users.noreply.github.com>
2024-10-24 18:50:22 -0400

    Add files via upload
    Pokémon Card Game

---
+++ Card Game/Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon extends Card{
+    private int hp;
+
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int userInputHP){
+        hp=userInputHP;
+    }
+}
