51230112f91d28e1f0b6b09c2e80669a8d2a3f07
wolferin12 <95312861+wolferin12@users.noreply.github.com>
2024-11-01 16:22:23 -0400

    Update BirthdayTester.java

--- Birthday Paradox/BirthdayTester.java
+++ Birthday Paradox/BirthdayTester.java
@@ -1,14 +1,17 @@
 
 
 public class BirthdayTester{
+    // Tests to see if there are any hits of a shared birthday in the class
     private static boolean hasBirthdayMatch(int classNumber){
+        //Array to hold the people in the room
         Person[] room = new Person[classNumber];
+        //Array that tracks what birthdays have been checked
         boolean [] birthday= new boolean[365];
-
+        // Iterates through each person in the class
         for (int i=0; i<classNumber; i++){
             room [i]= new Person();
             int birth= room[i].birthday;
-
+           //Checks if the birthday has already been checked
             if (birthday[birth-1]){
                 return true;
             }else {
@@ -18,15 +21,17 @@ public class BirthdayTester{
         return false;
     }
     public static void main(String[] args) {
-      int classNumber= 35;
-      int trial= 10;
+      int classNumber= 35; //Number of people in the room
+      int trial= 10; //Number of trials
 
       int collection=0;
+      //Runs ,ultiple trials to estimate the probability  
       for (int i=0; i<trial; i++){
           if(hasBirthdayMatch(classNumber)){
               collection++;
           }
       }
+     //Calculates the probability
       double prob = (double) collection/trial;
 
       System.out.println("The Probability of two people sharing a birthday in a room of "+classNumber+": ");
