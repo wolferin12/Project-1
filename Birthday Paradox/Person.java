fe3bbd1ad82817656591d74602a0f4f79d44a817
wolferin12 <95312861+wolferin12@users.noreply.github.com>
2024-11-01 16:23:51 -0400

    Update Person.java

--- Birthday Paradox/Person.java
+++ Birthday Paradox/Person.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class Person  {
-    int birthday;
+    int birthday; //Stores the birthday
 
-
+//Assigns a random birthday
     public Person (){
         Random var = new Random();
+        // Assigns a ranom birthday 
         birthday= var.nextInt(365)+1;
     }
 }
