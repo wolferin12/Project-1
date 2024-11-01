5429918aa520bda2a82be9cc16f701d4df10ec03
wolferin12 <95312861+wolferin12@users.noreply.github.com>
2024-11-01 16:49:09 -0400

    Update MontyHall.java

--- MontyHall.java
+++ MontyHall.java
@@ -2,20 +2,25 @@ import java.util.Random;
 
 public class MontyHall {
     public static void main(String[] args) {
-        int rounds=10000;
+        int rounds=10000; // Number of simmulations
         int switchWins=0;
         int noSwitchWins=0;
+        // Random number generator that simulates door choices
         Random doorsGenerator= new Random();
-
+        // Loop through each round 
         for (int i=0; i<rounds; i++){
+            // Assigns a car behind one of the three doors
             int car= doorsGenerator.nextInt(3);
-
+            // contestant then makes a choice 
             int doorchoice= doorsGenerator.nextInt(3);
             int goat;
+            //Determines whether the door with the goat was chosen or not
             do {
                 goat= doorsGenerator.nextInt(3);
             } while (goat ==car||goat==doorchoice);
+            //Switch to the remaining door
             int doorSwitch= 3-doorchoice-goat;
+            // Checks to see if switching the door wins the car
             if (doorSwitch==car){
                 switchWins++;
             } else if (doorchoice == car){noSwitchWins++;}
